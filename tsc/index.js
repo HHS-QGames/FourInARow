911f0e008d124a7f3a558e4740c7af02849d6238
treinDotDev <thomasbruijs@live.nl>
2024-11-23 20:38:28 +0100

    Updates

--- tsc/index.js
+++ tsc/index.js
@@ -6,5 +6,7 @@ import Home from './components/Home';
 import { LocalGame } from './components/LocalGame';
 import { OnlineGame1 } from './components/OnlineGame1';
 import { OnlineGame2 } from './components/OnlineGame2';
+// This is some ugly code to get the basename working in the GitHub pages deployment... sorry
+const basename = !process.env.REACT_APP_USE_BASENAME ? 'FourInARow' : undefined;
 const root = createRoot(document.getElementById('react'));
-root.render(_jsx(Router, { children: _jsx("div", { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/local", element: _jsx(LocalGame, {}) }), _jsx(Route, { path: "/online", element: _jsx(OnlineGame1, {}) }), _jsx(Route, { path: "/help", element: _jsx(Help, {}) }), _jsx(Route, { path: "/:opponent", element: _jsx(OnlineGame2, {}) })] }) }) }));
+root.render(_jsx(Router, { basename: basename, children: _jsx("div", { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/local", element: _jsx(LocalGame, {}) }), _jsx(Route, { path: "/online", element: _jsx(OnlineGame1, {}) }), _jsx(Route, { path: "/help", element: _jsx(Help, {}) }), _jsx(Route, { path: "/:opponent", element: _jsx(OnlineGame2, {}) })] }) }) }));
