e8a2e73a23b24b41a784e6a301d6c03af49fe3d3
treinDotDev <thomasbruijs@live.nl>
2024-12-07 18:07:46 +0100

    Add webpack dev server and update build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,60 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './dist/tsc/index.js',
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/webpack/FourInARow'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/FourInARow/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // CSS loaders
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource", // Asset loader for images
+        generator: {
+          filename: "images/[name][ext]", // Output path for images
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      base: "/FourInARow/",
+    }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'style', to: 'style' },
-        { from: 'images', to: 'images' },
-        { from: 'index.html', to: 'index.html' },
-        { from: '404.html', to: '404.html' },
+        { from: "style", to: "style" },
+        { from: "images", to: "images" },
       ],
     }),
   ],
+  devServer: {
+    static: "./dist",
+    compress: true,
+    port: 9876,
+    historyApiFallback: {
+      index: "/FourInARow/",
+    },
+    open: {
+      target: "http://localhost:9876/FourInARow", // Open the browser at this path
+    },
+  },
+  mode: "development",
 };
