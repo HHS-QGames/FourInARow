36e0693b64a04515d3f1ec39889e7dcf9264b15a
treinDotDev <thomasbruijs@live.nl>
2024-12-07 14:25:21 +0100

    Update the building and deployment to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './dist/tsc/index.js',
@@ -6,5 +7,14 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'style', to: 'style' },
+        { from: 'images', to: 'images' },
+        { from: 'index.html', to: 'index.html' },
+        { from: '404.html', to: '404.html' },
+      ],
+    }),
+  ],
 };
